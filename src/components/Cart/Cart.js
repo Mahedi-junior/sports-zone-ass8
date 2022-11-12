6c804fd30bf489b5a8bbd358a77818eb4000bebc
Mahedi-junior <mahedih.cse@gmail.com>
2022-11-12 18:17:35 +0600

    time calculation completed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,10 +2,17 @@ import React from "react";
 import "./Cart.css";
 
 const Cart = (props) => {
-  //   console.log(props);
+  const { cart } = props;
+  //   console.log(cart);
+
+  let time = 0;
+  for (const game of cart) {
+    time = time + game.time;
+  }
+
   return (
-    <div>
-      <h2>History: {props.cart.length}</h2>
+    <div className="admin-board">
+      <h2>History: {cart.length}</h2>
       <div className="admin-info">
         <div className="info">
           <p>76kg</p>
@@ -27,9 +34,11 @@ const Cart = (props) => {
         <p>30m</p>
         <p>40m</p>
       </div>
-      <h3>Exercice Details</h3>
+      <h3>Exercise Details</h3>
       <div className="ex-time">
-        <p>Exercise time:</p>
+        <p>
+          Playing time: <span className="total-time">{time}min</span>
+        </p>
         <p>Break time:</p>
       </div>
       <button className="complete-btn">Activity Completed</button>
