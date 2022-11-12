16453ba85c3da15d6d851ad039ea168b272dba43
Mahedi-junior <mahedih.cse@gmail.com>
2022-11-12 15:19:21 +0600

    admin cart added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,36 @@
 import React from "react";
+import "./Cart.css";
 
 const Cart = () => {
   return (
     <div>
-      <h1>This is Cart</h1>
+      <div className="admin-info">
+        <div className="info">
+          <p>76kg</p>
+          <p>Weight</p>
+        </div>
+        <div className="info">
+          <p>6.5</p>
+          <p>Height</p>
+        </div>
+        <div className="info">
+          <p>34yer</p>
+          <p>Age</p>
+        </div>
+      </div>
+      <h3>Add a Break</h3>
+      <div className="time">
+        <p>10m</p>
+        <p>20m</p>
+        <p>30m</p>
+        <p>40m</p>
+      </div>
+      <h3>Exercice Details</h3>
+      <div className="ex-time">
+        <p>Exercise time:</p>
+        <p>Break time:</p>
+      </div>
+      <button className="complete-btn">Activity Completed</button>
     </div>
   );
 };
